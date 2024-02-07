7cfa466d703b6148c06958db4484199765355939
Andrii Klymiuk <gradli.inc@gmail.com>
2024-02-07 16:42:04 +0200

    Initial commit for rest for todo

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+interface TodoItem {
+  id: string;
+  label: string;
+  checked?: boolean;
+}
+
+const todos: TodoItem[] = [];
+
+// Logging middleware
+app.use('*', async (c, next) => {
+  const start = Date.now();
+  await next();
+  const duration = Date.now() - start;
+
+  console.log(`${c.req.method} ${c.req.url} ${c.res.status} - ${duration}ms`);
+});
+
+// CORS middleware for local development
+app.use('*', async (c, next) => {
+  c.res.headers.set('Access-Control-Allow-Origin', '*');
+  c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  c.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (c.req.method === 'OPTIONS') {
+    return new Response('OK', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
+  }
+
+  await next();
+});
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/todos', async (c) => {
+  const todo = await c.req.json();
+  todos.push(todo);
+  return c.json(todo);
+});
+
+app.delete('/todos/:id', (c) => {
+  const { id } = c.req.param();
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index > -1) {
+    todos.splice(index, 1);
+    return c.json({ message: 'Todo removed' });
+  }
+  return c.json({ message: 'Todo not found' }, 404);
+});
+
+app.put('/todos/:id', async (c) => {
+  const { id } = c.req.param();
+  const todoUpdate = await c.req.json();
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index > -1) {
+    todos[index] = { ...todos[index], ...todoUpdate };
+    return c.json(todos[index]);
+  }
+  return c.json({ message: 'Todo not found' }, 404);
+});
+
+app.patch('/todos/:id/toggle', (c) => {
+  const { id } = c.req.param();
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index > -1) {
+    todos[index].checked = !todos[index].checked;
+    return c.json(todos[index]);
+  }
+  return c.json({ message: 'Todo not found' }, 404);
+});
+
+app.get('/todos', (c) => {
+  return c.json(todos);
+});
+
+export default {
+  port: process.env.PORT || 3000,
+  fetch: app.fetch,
+}
